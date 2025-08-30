f4ce0698f69d772181307b51ef8e0f2ad4b2b21e
tovtoken-art <tovtoken@gmail.com>
2025-08-31 06:52:22 +0900

    2
    2

---
+++ c3c-dashboard-fixed2/app/RealtimeKick.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { supabase as sbClient } from "../lib/supabase";
+import { useEffect } from "react";
+
+export default function RealtimeKick() {
+  useEffect(() => {
+    const ch = sbClient
+      .channel("te_insert")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "trade_events" },
+        () => window.location.reload()
+      )
+      .subscribe();
+    return () => { sbClient.removeChannel(ch); };
+  }, []);
+  return null;
+}
