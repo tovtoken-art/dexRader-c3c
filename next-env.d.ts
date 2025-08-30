d679f06a76cf7dba528659344201638fd4c3731a
tovtoken-art <tovtoken@gmail.com>
2025-08-31 08:22:58 +0900

    14

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,5 @@
-// next-env.d.ts
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-// NOTE: 이 파일은 수정하지 마세요. Next.js가 타입을 자동으로 주입합니다.
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
